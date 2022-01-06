a70b5bd8b8c559fade3bb1e6c8ce01e47a1ae5d2
Robert Irvine <robert@seamlessml.com>
2022-01-06 16:51:05 +0000

    update

--- chai_py/bots.py
+++ chai_py/bots.py
@@ -1,5 +1,11 @@
 from enum import Enum
 
+from chai_py.auth import get_auth
+from chai_py import types
+import requests
+
+URL = 'http://chaiserver2.cmlsml.com'
+
 
 def get_bot(bot_uid):
     """
@@ -8,4 +14,9 @@ def get_bot(bot_uid):
     :param bot_uid: Bot UID
     :returns: DeployedBot
     """
-    pass
+    auth = auth.get_auth()
+    credentials = requests.HTTPBasicAuth(auth.key, auth.key)
+    url = '{}/chatbots/{}'.format(bot_uid)
+    resp = requests.get(url, auth=credentials)
+    assert resp.status_code == 20, resp.text
+    return types.DeployedBot.from_json(resp.json())
